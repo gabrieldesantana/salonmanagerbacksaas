7b6735948e19b92175ef565751df23e3625f4fd1
Gabriel de Santana Gomes <1202105723@acd.grupotiradentes.com>
2023-10-30 21:21:11 -0300

    Ajusta os agendamentos

--- src/SalonManager.Domain/Enums/EAppointmentStatus.cs
+++ src/SalonManager.Domain/Enums/EAppointmentStatus.cs
@@ -6,8 +6,8 @@
         Iniciado = 2,
         Cancelado = 3,
         Adiado = 4,
-        Realizado = 5,
-        Pendente_ = 6,
-        Finalizado = 7
+        //Realizado = 5,
+        Pendente_ = 5,
+        Finalizado = 6
     }
 }
